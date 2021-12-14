f6beced10719e29a065d5df22ab80dc7f60870a6
hectorzaragoza <zaragoza09@gmail.com>
2021-12-13 19:21:57 -0600

    Added Delete Route

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const mongooseBaseName = 'crypto-bytes'
 
 // create the mongodb uri for development and test
 const database = {
-	development: `mongodb://127.0.0.1:27017/${mongooseBaseName}-development`,
+	development: `mongodb://localhost:27017/${mongooseBaseName}-development`,
 	test: `mongodb://localhost/${mongooseBaseName}-test`,
 }
 
